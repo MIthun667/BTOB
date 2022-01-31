4b018d1c6383da1b160db8f168f28c3f3a01375d
Md.Mithun Hossain <mhosen751@gmail.com>
2022-01-31 21:32:15 +0600

    first commit'

---
+++ src/Shared/Navbar/Navitems/Navitems.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+const pages = [ 'Product', 'Solutions', 'Resources', 'Pricing' ];
+
+const productItem = [ 'Overview', 'Compare Us']
+
+function Navitems() {
+    const [ products, setProducts ] = 
+    const handleOpenProduct = e =>{
+        set
+    }
+  return (
+      <div>
+
+      </div>
+  );
+}
+
+export default Navitems;
